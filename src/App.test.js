87c224ab0f117616e11465e63e5c2a6f05d51010
Steve <steve@LBG.com>
2025-04-09 13:42:58 +0100

    Change file name to included Steve

--- src/App.test.js
+++ src/App.test.js
@@ -9,6 +9,6 @@ import App from './App';
 
 test('renders VAT Calculator text', () => {
   render(<App />);
-  const heading = screen.getByText(/VAT CALCULATOR/i);
+  const heading = screen.getByText(/VAT CALCULATOR - Steve/i);
   expect(heading).toBeInTheDocument();
 });
